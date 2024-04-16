4ca631d97eaf9d925eb90166985afe775d9b66f2
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-16 17:28:52 +0300

    feat(controller): add createTodo function
    createTodo function adds a new object to the todos array with the title and priority received from the user and the other fields like the id, createdAt, done and doneAt.
    
    See #2

---
+++ backend/controller/todo.Controller.js
@@ -0,0 +1,18 @@
+const todoList = []; //this array will hold an item in the TODO list.
+
+//createTodo function will add a new object to the todos array.
+exports.createTodoList = (req, res) => {
+    //when creating a new TODO, the user sends the title and priority of the TODO in the request body.
+    const { title, priority } = req.body; 
+    //create a new object with the title and priority received from the user.
+    const newTodoList = {
+        id: todoList.length + 1, // simple ID assignment
+        title: title,
+        priority: priority,
+        createdAt: new Date(),
+        done: false, // when creating a new TODO, it is not done yet.
+        doneAt: null
+    };
+    todoList.push(newTodoList);
+    res.status(201).send(newTodoList);
+};
