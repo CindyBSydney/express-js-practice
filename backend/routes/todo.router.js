4acf22b9c1544ceecb4aba2421ae0c389a76e996
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-16 18:03:14 +0300

    feat(routes): add DELETE route
    See #2

--- backend/routes/todo.router.js
+++ backend/routes/todo.router.js
@@ -5,6 +5,7 @@ const todoController = require('../controller/todo.Controller');
 router.post('/todo', todoController.createTodoList);
 router.get('/todo', todoController.getTodoList);
 router.put('/todo/:id', todoController.updateTodo);
+router.delete('/todo/:id', todoController.deleteTodo);
 
 
 module.exports = router;
