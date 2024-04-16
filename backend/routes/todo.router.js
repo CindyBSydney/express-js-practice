0a8135be520a84da132fb7ce4422303b496e1fb4
CindyBSydney <cindy.bosibori@strathmore.edu>
2024-04-16 17:40:44 +0300

    feat(routes): add GET route
    added the GET route to read items in the todo list
    
    See #2

--- backend/routes/todo.router.js
+++ backend/routes/todo.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const todoController = require('../controller/todo.Controller'); 
 
 router.post('/todo', todoController.createTodoList);
+router.get('/todo', todoController.getTodoList);
 
 
 module.exports = router;
